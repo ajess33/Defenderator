bb78742b67d2aaa5d7cc993fe78d9787fce90fc3
Austin Jess <ajess33@gmail.com>
2019-04-02 12:55:27 -0500

    added render function to constructor

--- js/constructor.js
+++ js/constructor.js
@@ -23,7 +23,7 @@ function MakeMonster(monsterType, name, increment) {
   this.hitbonus = this.strMod + monsterType.proficiency;
   this.proficiency = monsterType.proficiency;
   this.averagedamage = monsterType.averagedamage;
-  this.increment = increment;
+  this.increment = parseInt(increment);
 
   // push each new monster to an array
   MakeMonster.all.push(this);
@@ -69,20 +69,43 @@ MakeMonster.prototype.baseStatUp = function() {
   }
 };
 
-MakeMonster.prototype.totalhealth = function () {
-  this.averagehp = this.averagehp + Math.ceil((this.averagehp * .5 * increment) + (this.conMod * (increment + 1)));
+MakeMonster.prototype.totalhealth = function() {
+  this.averagehp =
+    this.averagehp +
+    Math.ceil(
+      this.averagehp * 0.5 * this.increment + this.conMod * (this.increment + 1)
+    );
 };
 
-MakeMonster.prototype.totaldamage = function () {
-  this.averagedame = this.averagedamage + Math.ceil((this.averagedamage * .25 * increment) + (this.strMod * (increment +1)));
+MakeMonster.prototype.totaldamage = function() {
+  this.averagedame =
+    this.averagedamage +
+    Math.ceil(
+      this.averagedamage * 0.25 * this.increment +
+        this.strMod * (this.increment + 1)
+    );
 };
 
-MakeMonster.prototype.newProficiency = function () {
-  this.proficiency = this.proficiency + Math.floor((this.proficiency * .25 * increment));
+MakeMonster.prototype.newProficiency = function() {
+  this.proficiency =
+    this.proficiency + Math.floor(this.proficiency * 0.25 * this.increment);
 };
 
-MakeMonster.prototype.newArmorClass = function () {
-  this.armorclass = this.armorclass + (this.dexMod *(increment+1)) + Math.floor((increment *.5));
+MakeMonster.prototype.newArmorClass = function() {
+  this.armorclass =
+    this.armorclass +
+    this.dexMod * (this.increment + 1) +
+    Math.floor(this.increment * 0.5);
+};
+
+MakeMonster.prototype.render = function() {
+  console.log(this);
+  this.baseStatUp();
+  this.totalHealth();
+  this.totaldamage();
+  this.newProficiency();
+  this.newArmorClass();
+  console.log(this);
 };
 
 var elName = document.getElementById('name-select');
@@ -107,6 +130,7 @@ function handleSubmit(e) {
     levelSelected
   );
 
+  newMonster.render();
   displayChart(newMonster);
 }
 
@@ -115,19 +139,24 @@ function displayChart(monster) {
   var canvas = document.getElementById('monster-graph').getContext('2d');
 
   var labelsArray = Object.keys(monster);
+  labelsArray.splice(16, 1);
+  labelsArray.splice(0, 1);
   var valuesArray = Object.values(monster);
-  console.log(labelsArray);
-  console.log(valuesArray);
+  valuesArray.splice(16, 1);
+  valuesArray.splice(0, 1);
 
-  // var chart = new Chart(canvas, {
-  //   type: 'bar',
+  var healthChart = new Chart(canvas, {
+    type: 'bar',
 
-  //   data: {
-  //     labels: labelsArray,
-  //     datasets: {
-  //       label: 'Monster Stats',
-  //       data: valuesArray
-  //     }
-  //   }
-  // });
+    data: {
+      labels: 'Health',
+      datasets: [
+        {
+          label: 'Total Health',
+          backgroundColor: '#a30a08',
+          data: valuesArray
+        }
+      ]
+    }
+  });
 }
