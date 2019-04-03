b5e90408cd8a50103c8f2c95b7cd2e3f7b3f1c20
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-04-03 14:41:52 -0500

    fixed so all monsters work with scaling

--- js/userMonsters.js
+++ js/userMonsters.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var userMonsters = {};
-
 function loadUserMonsters(){
-  userMonsters = JSON.parse(localStorage['newMonster'])
+    if(localStorage['newMonster']){
+        monsters = JSON.parse(localStorage['newMonster'])
+    }
+  
 };
 
 function populateDropdown() {
   console.log('i am running');
-  var userMonsterNames = Object.keys(userMonsters);
-  console.log(userMonsterNames)
+  var userMonsterNames = Object.keys(monsters);
   for(var i = 0; i < userMonsterNames.length; i++) {
     var newOption = document.createElement('option');
     newOption.textContent = userMonsterNames[i];
