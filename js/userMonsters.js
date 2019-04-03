cfbe9c9a6e0a25bf78e4ea127bf75074131dbc8d
Kyle Monahan <kyle.monahan2000@gmail.com>
2019-04-03 14:22:29 -0500

    populated dropbox from user created monsters

---
+++ js/userMonsters.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var userMonsters = {};
+
+function loadUserMonsters(){
+  userMonsters = JSON.parse(localStorage['newMonster'])
+};
+
+function populateDropdown() {
+  console.log('i am running');
+  var userMonsterNames = Object.keys(userMonsters);
+  console.log(userMonsterNames)
+  for(var i = 0; i < userMonsterNames.length; i++) {
+    var newOption = document.createElement('option');
+    newOption.textContent = userMonsterNames[i];
+    newOption.value = userMonsterNames[i];
+    var monsterSelect = document.getElementById('monster-select');
+    monsterSelect.appendChild(newOption);
+  }
+}
+
+loadUserMonsters();
+populateDropdown();
